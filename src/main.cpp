1907e3be1c093aad11478662b65c6019def307cf
Luke Hale <luahale@ucsc.edu>
2026-05-06 11:21:50 -0700

    added ticker for updating angle; update limits for pulse width

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "INA219.hpp"
 #include "TextLCD.h"
 #include "mbed_thread.h"
+#include <cmath>
 
 I2C i2c_lcd(D14, D15);                          // SDA, SCL
 TextLCD_I2C lcd(&i2c_lcd, 0x4E, TextLCD::LCD16x2);
@@ -19,50 +20,48 @@ AnalogIn ldr1(A0);
 AnalogIn ldr2(A1);
 INA219 ina219(D14, D15, 0x40, 400000, RES_10BITS);
 
-Ticker measure;
-Ticker m;
+Ticker t1;
+Ticker t2;
 
-volatile bool flag = false;
+volatile bool f1 = false;
 volatile bool f2 = false;
 float current_ma;
 float voltage;
 
-void tick()
+void update_angle()
 {
-    flag = true;
+    f1 = true;
 }
-void t2(){
+void update_lcd(){
     f2 = true;
 }
 
 int main() {
     srv.period_ms(20); // Set the PWM period to 20 ms (50 Hz)
     
-    float pulse = 1.75;
-    measure.attach(&tick, 10);
-    m.attach(&t2, 1);
+    float pulse = 1.5;
+    t1.attach(&update_angle, 0.3f);
+    t2.attach(&update_lcd, 2);
     lcd.setBacklight(TextLCD::LightOn);
     lcd.cls();
     while (true) {
+
         float val1 = ldr1.read();
         float val2 = ldr2.read();
 
-        if (fabs(val1 - val2) > 0.05) {
-            if (val1 > val2 && pulse < 2.5) {
-                pulse += 0.01;
+        if (fabs(val1 - val2) > 0.05 && f1) {
+            f1 = false;
+            if (val1 > val2 && pulse < 2.0) {
+                pulse += 0.05;
             } else if (val2 > val1 && pulse > 1.0) {
-             pulse -= 0.01;
+                pulse -= 0.05;
             }
-            srv.pulsewidth(pulse * 1e-3f);
 
+            srv.pulsewidth(pulse / 1000);
         }
 
-        printf("LDR1: %.3f, LDR2: %.3f\n", val1, val2);
-
-        wait_ms(20);
-
-        if (flag) {
-            flag = false;
+        if (f2) {
+            f2 = false;
 
             current_ma = ina219.read_current_mA();
             voltage = ina219.read_bus_voltage();
@@ -74,5 +73,6 @@ int main() {
             lcd.locate(0, 1);
             lcd.printf("vol: %.2f V  ", voltage);
         }
+
     }
 }
